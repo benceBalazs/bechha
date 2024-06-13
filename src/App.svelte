4f03de7b61f6ed885fe7d1430c7d759fdbf1ba97
Bence Balazs <56939786+benceBalazs@users.noreply.github.com>
2024-06-14 00:32:48 +0200

    draft login

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import LoginWidget from "./lib/LoginWidget.svelte";
+</script>
+
+<main class="grid grid-rows-4 h-full w-full font-inter">
+	<div class="w-full h-full row-span-1 place-content-center">
+		<div class="w-full h-fit">
+			<h1 class="text-center text-5xl font-thin">bechha</h1>
+		</div>
+	</div>
+	<div class="w-full h-full row-span-3 grid place-content-center">
+		<LoginWidget />
+	</div>
+</main>
---
+++ src/lib/LoginWidget.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<div class="w-fit h-fit px-10 py-5 rounded border-black border-x border-y bg-white shadow-2xl grid grid-rows-6 gap-4">
+  <div class="row-span-1 grid place-content-center">
+    <h2 class="text-2xl font-extralight">DRES Interface</h2>
+  </div>
+  <div class="row-span-4 grid grid-rows-2">
+    <div class="flex flex-col">
+      <label class="text-lg font-extralight" for="username">Username</label>
+      <input class="rounded border-black border-x border-y" type="text" name="username" id="username">
+    </div>
+    <div class="flex flex-col">
+      <label class="text-lg font-extralight" for="password">Password</label>
+      <input class="rounded border-black border-x border-y" type="password" name="password" id="password">
+    </div>
+  </div>
+  <div class="row-span-1 grid place-content-center">
+    <input class="text-lg font-extralight px-2 py-1 rounded border-black border-x border-y hover:cursor-pointer hover:shadow hover:bg-gray-300" type="button" value="Log in" on:click={()=>alert("Hellow")}>
+  </div>
+</div>
